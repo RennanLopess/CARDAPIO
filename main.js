b8109af39b3cfd0136d9e8436acf662fabf2163e
Rennan Rentz <rennanrentz@gmail.com>
2025-03-14 20:30:50 -0300

    Inicialização com JavaScript

--- main.js
+++ main.js
@@ -2,4 +2,4 @@
 const botoesAdicionar = document.querySelectorAll(".adicionar");
 const listaPedido     = document.getElementById("lista-pedido");
 const totalElemento   = document.getElementById("total");
-let total = 0;
+let total             = 0;
