a1243568b97bf7bb3818f2e6133f1453b34edb53
Rennan Rentz <rennanrentz@gmail.com>
2025-03-14 20:29:32 -0300

    Inicialização com javaScript

---
+++ main.js
@@ -0,0 +1,5 @@
+// SIMULAÇÃO DE ADIÇÃO DE PRODUTOS Á CESTA DO SITE.
+const botoesAdicionar = document.querySelectorAll(".adicionar");
+const listaPedido     = document.getElementById("lista-pedido");
+const totalElemento   = document.getElementById("total");
+let total = 0;
